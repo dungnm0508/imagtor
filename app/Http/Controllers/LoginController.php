59af960d32dd3f80e86eaf6ce553d2c870566852
dungnm <dungnm@zotaobox.com>
2019-01-25 10:22:59 +0700

    ad

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function getLoginForm(){
+    	return view('auth/loginForm');
+    }
+}
